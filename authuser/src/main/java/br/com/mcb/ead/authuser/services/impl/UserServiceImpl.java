3029df711c63b085d304c3dc9bb3cbedfae55665
cxmarcelo <marcelo.c.b22@gmail.com>
2024-03-20 22:47:16 -0300

    Deleção em cascada parte 2

--- authuser/src/main/java/br/com/mcb/ead/authuser/services/impl/UserServiceImpl.java
+++ authuser/src/main/java/br/com/mcb/ead/authuser/services/impl/UserServiceImpl.java
@@ -9,7 +9,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.mcb.ead.authuser.clients.CourseClient;
 import br.com.mcb.ead.authuser.models.UserCourseModel;
 import br.com.mcb.ead.authuser.models.UserModel;
 import br.com.mcb.ead.authuser.repositories.UserCourseRepository;
@@ -21,9 +23,12 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private UserCourseRepository userCourseRepository;
+
+	@Autowired
+	private CourseClient courseClient;
 
 	@Override
 	public List<UserModel> findAll() {
@@ -35,13 +40,20 @@ public class UserServiceImpl implements UserService {
 		return userRepository.findById(userId);
 	}
 
+	@Transactional
 	@Override
 	public void delete(UserModel userModel) {
+		boolean deleteUserCourseInCourse = false;
 		List<UserCourseModel> userCourseModelList = userCourseRepository.findAllUserCourseIntoUser(userModel.getUserId());
 		if(!userCourseModelList.isEmpty()) {
 			userCourseRepository.deleteAll(userCourseModelList);
+			deleteUserCourseInCourse = true;
 		}
+
 		userRepository.delete(userModel);
+		if(deleteUserCourseInCourse) {
+			courseClient.deleteUserInCourse(userModel.getUserId());
+		}
 	}
 
 	@Override
